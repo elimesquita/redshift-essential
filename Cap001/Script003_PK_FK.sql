2a39af11db01afbfc94e544471e43d67d20d49be
Eliezio Mesquita <87777462+elimesquita@users.noreply.github.com>
2023-08-24 05:40:46 -0300

    Atualizado em 24/08

--- Cap001/Script003_PK_FK.sql
+++ Cap001/Script003_PK_FK.sql
@@ -11,17 +11,17 @@ CREATE TABLE usuario(
     email_usuario varchar(120),
     id_departamento int,
 FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento)
-)
+);
 
 --Sintaxe 002
-CREATE TABLE usuario(
-    id_usuario int PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS usuario(
+    id_usuario int ,
     nome_usuario varchar(120),
     email_usuario varchar(120),
     id_departamento int,
+PRIMARY KEY (id_usuario),
 FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento)
 );
-
 
 --Validando as tabelas
 SELECT * FROM departamento;
@@ -30,19 +30,50 @@ SELECT * FROM usuario;
 
 --Inserindo Dados nas Tabelas:
 --Tabela de Departamento
+
+--Tabela de Departamento - Opção 01
 INSERT INTO departamento(id_departamento,nome_departamento) VALUES(1,'Engenharia de Dados');
 INSERT INTO departamento(id_departamento,nome_departamento) VALUES(1,'Arquitetura de Dados');
-UPDATE departamento SET id_departamento = 2 WHERE nome_departamento = 'Arquitetura de Dados';
+
 INSERT INTO departamento(id_departamento,nome_departamento) VALUES(3,'Ciência de Dados');
 INSERT INTO departamento VALUES(4,'Devops');
 INSERT INTO departamento VALUES(5,'Vendas');
 
---Tabela de Usuário
-INSERT INTO usuario(id_usuario, nome_usuario, email_usuario,id_departamento) VALUES (1,'Eliézio Mesquita', 'eliezio.mesquita@ia16.com.br','1');
-INSERT INTO usuario(id_usuario, nome_usuario, email_usuario,id_departamento) VALUES (1,'Jose Silva', 'eliezio.mesquita@ia16.com.br','2');
-INSERT INTO usuario(id_usuario, nome_usuario, email_usuario,id_departamento) VALUES (1,'Carlos Souza', 'eliezio.mesquita@ia16.com.br','3');
-INSERT INTO usuario VALUES (1,'Maria Mendes', 'eliezio.mesquita@ia16.com.br','4');
-INSERT INTO usuario VALUES (1,'João Machado', 'eliezio.mesquita@ia16.com.br','6');
+--Tabela de Departamento - Opção 02
+INSERT INTO departamento(id_departamento,nome_departamento)
+VALUES(1,'Engenharia de Dados'),
+      (1,'Arquitetura de Dados'),
+      (3,'Ciência de Dados'),
+      (3,'Ciência de Dados'),
+      (4,'Devops'),
+      (5,'Vendas');
+
+--Tabela de Usuário - Opção 01
+INSERT INTO usuario(id_usuario, nome_usuario, email_usuario,id_departamento) VALUES (1,'Eliezio Mesquita', 'eliezio.mesquita@ia16.com.br','1');
+INSERT INTO usuario(id_usuario, nome_usuario, email_usuario,id_departamento) VALUES (1,'Jose Silva', 'jose.silva@ia16.com.br','2');
+INSERT INTO usuario(id_usuario, nome_usuario, email_usuario,id_departamento) VALUES (1,'Carlos Souza', 'calos.souza@ia16.com.br','3');
+INSERT INTO usuario VALUES (4,'Maria Mendes', 'maria.mendes@ia16.com.br','4');
+INSERT INTO usuario VALUES (5,'Joao Machado', 'joao.machado@ia16.com.br','6');
+
+
+--Tabela de Usuário - Opção 02
+INSERT INTO usuario(id_usuario, nome_usuario, email_usuario,id_departamento) 
+VALUES (1,'Eliezio Mesquita', 'eliezio.mesquita@ia16.com.br','1'),
+       (1,'Jose Silva', 'jose.silva@ia16.com.br','2'),
+       (1,'Carlos Souza', 'calos.souza@ia16.com.br','3'),
+       (4,'Maria Mendes', 'maria.mendes@ia16.com.br','4'),
+        (5,'Joao Machado', 'joao.machado@ia16.com.br','6');
+
+
+--Atualizando Dados
+-- Atualizando uma coluna por update
+UPDATE departamento SET id_departamento = 2 WHERE nome_departamento = 'Arquitetura de Dados';
+UPDATE usuario SET id_usuario=2 WHERE nome_usuario = 'Jose Silva';
+
+-- Atualizando mais de uma coluna por update
+UPDATE usuario
+SET id_usuario = 3, email_usuario = 'carlos.souza@ia16.com.br'
+WHERE nome_usuario = 'Carlos Souza'; 
 
 ## Importante
 As restrições de chave externa são somente informativas. 
@@ -66,11 +97,14 @@ WHERE constraint_schema ='public'
 AND table_name = 'usuario';
 
 -- Deletando Linhas
-
+DROP TABLE departamento;
 DELETE FROM departamento where id_departamento = 1;
 SELECT * FROM departamento;
 
 -- Drop e Validação de Constraint
+ALTER TABLE usuario DROP CONSTRAINT usuario_id_departamento_fkey;
+
+-- Drop de Tabela e Constraint
 DROP TABLE departamento;
 DROP TABLE departamento CASCADE;
 
